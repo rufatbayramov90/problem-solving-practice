22773c091d900c93777ffbad276f77cd4c5f8ca9
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-11-13 00:34:13 +0400

    another metod

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const str = "Give me $20'";
 console.log(str.replace(/[^a-zA-Z ]/g, ""));
 
+
+// Diger bir forma
+function remove(x) {
+    let lower = x.toLowerCase();
+    let upper = x.toUpperCase();
+
+    let result = "";
+    for(var i=0; i<lower.length; ++i) {
+        if(lower[i] != upper[i])
+            result += x[i];
+    }
+    return result;
+}
+console.log(remove('Give me $20'))
